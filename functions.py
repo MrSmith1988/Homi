e6563eb1ca536ed4e10143f437d01bff5164f6ef
MrSmith1988 <MrSmith1988@users.noreply.github.com>
2023-12-07 13:14:57 +1100

    moved more functionality to functions for efficiency and ease of maintenance

--- functions.py
+++ functions.py
@@ -1,13 +1,36 @@
 from microbit import *
-def recieveBlink(image):
-    def flashImage(image, time):
-            display.show(image)
-            sleep(time)
-            display.clear()
-            sleep(time)
-    flashImage(image, 200)
-    flashImage(image, 200)
-    flashImage(image, 400)
-    flashImage(image, 200)
-    flashImage(image, 200)
-    flashImage(image, 400)
+import radio
+
+arrows = Image('00000:'
+               '09090:'
+               '90009:'
+               '09090:'
+               '00000:')
+
+def flashFlash(image, numTimes):
+    def oneFlash(image, time):
+        display.show(image)
+        sleep(time)
+        display.clear()
+        sleep(time)
+    for i in range(numTimes):
+        oneFlash(image, 200)
+        oneFlash(image, 200)
+        oneFlash(image, 400)
+
+def move(choiceList, choiceNum, direction):
+    if direction == 'right':
+        if choiceNum == len(choiceList)-1:
+            return 0
+        else:
+            return choiceNum + 1
+    if direction == 'left':
+        if choiceNum == 0:
+            return len(choiceList)-1
+        else:
+            return choiceNum - 1
+        
+def sending(recipient, sender, image):
+    radio.send(recipient+" "+sender)
+    flashFlash(image, 1)
+    flashFlash(Image.ARROW_E, 1)
--- main.py
+++ main.py
@@ -1,51 +1,27 @@
 from microbit import *
 import radio
 from functions import *
-arrows = Image('00000:'
-               '09090:'
-               '90009:'
-               '09090:'
-               '00000:')
 
-selfID = 'Aditya'           #change to own name
-IDNo = 0
+selfID = 'Smith'           #change to own name
+IDNum = 0
 ID = ['Aditya', 'Brishti', 'Corrine', 'Harry', 'Hasini', 'Jaide', 'Kingston', 'Pei', 'Pratham', 'Ron', 'Rupert', 'Smith']
 
 radio.on()
 while True:
     display.show(arrows)
     if button_b.was_pressed():           #scrolls right
-        if IDNo == len(ID)-1:
-            IDNo = 0
-        else:
-            IDNo = IDNo + 1
-        display.scroll(ID[IDNo], delay=50)
+        IDNum = move(ID, IDNum, 'right')
+        display.scroll(ID[IDNum], delay=50)
     elif button_a.was_pressed():         #scrolls left
-        if IDNo == 0:
-            IDNo = len(ID)-1
-        else:
-            IDNo = IDNo - 1
-        display.scroll(ID[IDNo], delay=50)
+        IDNum = move(ID, IDNum, 'left')
+        display.scroll(ID[IDNum], delay=50)
     elif accelerometer.get_x() > 2010:            #starts the sending function and animation
-        display.scroll(ID[IDNo], delay=50)
-        radio.send(ID[IDNo]+" "+selfID)
-        display.show(Image.HEART)
-        sleep(500)
-        display.show(Image.ARROW_E)
-        sleep(500)
-        display.clear()
-        sleep(250)
-        display.show(Image.ARROW_E)
-        sleep(500)
-        display.clear()
-        sleep(250)
-        display.show(Image.ARROW_E)
-        sleep(500)
-        display.clear()
+        display.scroll(ID[IDNum], delay=50)
+        sending(ID[IDNum], selfID, Image.HEART)
 
     message = radio.receive()
     if message != None:
         message = message.split()
         if message[0] == selfID:
-            recieveBlink(Image.HEART)
+            flashFlash(Image.HEART, 2)
             display.scroll(message[1], delay=50)
